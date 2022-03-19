fdb93b84c577f5ac92b7d7eb0b3732ea31e29648
Khater47 <s234019@studenti.polito.it>
2022-03-19 01:25:36 +0100

    finish exercise 2 on Film library

--- Lab 1/Film library.js
+++ Lab 1/Film library.js
@@ -3,7 +3,7 @@
 
 const dayjs = require('dayjs');
 
-function Film(id, title, favorite = false, watchDate = dayjs(), rating = 0){
+function Film(id, title, favorite = false, watchDate, rating){
     this.id = id;
     this.title = title;
     this.favorite = favorite;
@@ -11,20 +11,66 @@ function Film(id, title, favorite = false, watchDate = dayjs(), rating = 0){
     this.rating = rating;
 }
 
-function FilmLibrary(){
-    this.library = [];
-
-
-    this.addNewFilm = (film) => {
-        this.library.push(film);
+const printLibrary = (library) => {
+    console.log('***** List of films *****');
+    for(const film of library){
+        if(film.watchDate !== undefined){
+            console.log(`Id: ${film.id}, Title: ${film.title}, Favorite: ${film.favorite}, Watch date: ${film.watchDate.format('MMMM DD, YYYY')}, Score: ${film.rating}`);
+        }else{
+            console.log(`Id: ${film.id}, Title: ${film.title}, Favorite: ${film.favorite}, Watch date: ${film.watchDate}, Score: ${film.rating}`);
+        }   
     }
 }
 
-const pulpFiction = new Film(1, 'Pulp Fiction', true, dayjs('2022-03-10'), 5);
-const grams21 = new Film(2, '21 Grams', true, dayjs('2022-03-17'), 4);
+function FilmLibrary(){
+    this.library = [];
+
+    //addNewFilm
+    this.addNewFilm = (film) => {
+        this.library.push(film);
+    }
+
+    //sortByDate
+    this.sortByDate = () => {
+        const ordered = this.library.filter( film => film.watchDate !== undefined).sort( (a, b) => (a.watchDate.isAfter(b.watchDate) ? 1 : -1));
+        this.library.forEach( film => { if(film.watchDate === undefined) ordered.push(film)});
+        return ordered;
+    }
+
+    //deleteFilm
+    this.deleteFilm = (id) => {
+        const index = this.library.findIndex((film) => film.id === id);
+        return this.library.splice(index, 1);
+    }
+
+    //resetWatchedFilms
+    this.resetWatchedFilms = () => {
+        return [...this.library].map( film => film.watchDate = undefined);
+    }
+
+    //getRated
+    this.getRated = () =>{
+        return this.library.filter( film => film.rating !== undefined).sort((a,b) => (a-b));
+    }
+
+    //printList
+    this.printList = () =>{
+        console.log('***** List of films *****');
+        for(const film of this.library){
+            if(film.watchDate !== undefined){
+                console.log(`Id: ${film.id}, Title: ${film.title}, Favorite: ${film.favorite}, Watch date: ${film.watchDate.format('MMMM DD, YYYY')}, Score: ${film.rating}`);
+             }else{
+                console.log(`Id: ${film.id}, Title: ${film.title}, Favorite: ${film.favorite}, Watch date: ${film.watchDate}, Score: ${film.rating}`);
+            }   
+        }
+    }
+}
+
+const pulpFiction = new Film(1, 'Pulp Fiction', true, dayjs('March 10, 2022', 'MMMM DD, YYYY'), 5);
+const grams21 = new Film(2, '21 Grams', true, dayjs('March 17, 2022', 'MMMM DD, YYYY'), 4);
 const starWars = new Film(3, 'Star Wars', false);
 const matrix = new Film(4, 'Matrix', false);
-const shrek = new Film(5, 'Shrek', false, dayjs('2022-03-21'), 3);
+const shrek = new Film(5, 'Shrek', false, dayjs('March 21, 2022', 'MMMM DD, YYYY'), 3);
 
 const myFilmLibrary = new FilmLibrary();
 myFilmLibrary.addNewFilm(pulpFiction);
@@ -33,4 +79,11 @@ myFilmLibrary.addNewFilm(starWars);
 myFilmLibrary.addNewFilm(matrix);
 myFilmLibrary.addNewFilm(shrek);
 
-console.log(myFilmLibrary);
+
+myFilmLibrary.printList();
+printLibrary(myFilmLibrary.getRated());
+printLibrary(myFilmLibrary.sortByDate());
+myFilmLibrary.resetWatchedFilms();
+myFilmLibrary.printList();
+myFilmLibrary.deleteFilm(3);
+myFilmLibrary.printList();
